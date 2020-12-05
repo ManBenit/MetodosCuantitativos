1c1f9e1e1c0ea8cab1bff108b88a303ebe99561d
IvanNavi13 <ivannavi.13@hotmail.com>
2020-12-05 03:44:57 -0600

    Indi

---
+++ Individuo.cpp
@@ -0,0 +1,47 @@
+/*
+PROYECTO SEGUNDO PARCIAL. MÉTODOS CUANTITATIVOS PARA LA TOMA DE DESICIONES.
+AUTORES:
+    Benítez Morales Manuel Emilio
+    Castillo Rodríguez David Israel
+    García García Cano Ivan
+
+    3CM8
+
+Algoritmo genético para la maximización/minimización
+VERSIÓN: 1.0
+
+Diciembre 2020.
+*/
+
+#include "Individuo.h"
+Individuo::Indiviudo(Genotipo gen, Fenotipo fen){   //Constructor
+    
+}
+
+int Individuo::id(){
+    
+}
+
+Fenotipo Individuo::fenotipo(){
+    
+}
+
+Genotipo Individuo::genotipo(){
+    
+}
+
+void Individuo::mutar(int pos){
+
+}
+
+void Individuo::cruzarCon(Individuo individuo){
+
+}
+
+string Individuo::imprimir(){
+    cout << "Vn\tVector\tValor x\tValor y" << endl;
+}
+
+
+
+
