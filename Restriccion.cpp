d168ac3c4263dfabc5c910757a8044e085c1626d
ManBenit <benitez.emilio.morales@gmail.com>
2020-12-05 02:43:42 -0600

    REst.cpp

---
+++ Restriccion.cpp
@@ -0,0 +1,37 @@
+/*
+PROYECTO SEGUNDO PARCIAL. MÉTODOS CUANTITATIVOS PARA LA TOMA DE DESICIONES.
+AUTORES:
+    Benítez Morales Manuel Emilio
+    Castillo Rodríguez David Israel
+    García García Cano Ivan
+
+    3CM8
+
+Algoritmo genético para la maximización/minimización
+VERSIÓN: 1.0
+
+Diciembre 2020.
+*/
+
+#include "Restriccion.h"
+Restriccion::Restriccion(string expresion){
+
+}
+
+//getter del límite inferior
+Restriccion::int limInf(){
+
+}
+
+// getter del límite superior
+Restriccion::int limSup(){
+
+}
+
+Restriccion::bool evaluar(int individuo){
+
+}
+
+Restriccion::void defAtributos(){
+
+}
